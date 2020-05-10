0d665baf9b82956119484fa24d92ab1c3ed5ddbf
jin_gang <1441609949@qq.com>
2020-05-10 22:53:00 +0800

    docs: commit-msg注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,18 +5,16 @@ module.exports = {
       2,
       'always',
       [
-        'chore',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test'
+        'chore', // 新增依赖
+        'docs', // 文档
+        'feat', // 新特性
+        'fix', // bug修复
+        'perf', // 性能优化
+        'refactor', // 功能重构
+        'revert', // 代码回滚
+        'style', // 样式
+        'test' // 测试
       ]
     ]
-    // 'subject-full-stop': [0, 'never'],
-    // 'subject-case': [0, 'never']
   }
 }
